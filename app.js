a8cee008ae48a6da3dad4b7fc772e8392eda41ff
Umer Farooq <umerfarooq2821035@gmail.com>
2024-12-16 22:10:34 +0500

    Assignment 01 Completed

---
+++ app.js
@@ -0,0 +1,122 @@
+// ======================================== Chapter 01 ======================================== ✅
+// Chapter 01
+// Question 01
+
+// Answer
+
+// ===============> alert("Welcome To Our Website");
+
+
+// Question 02
+// Answer
+
+// ===============> alert("Error! Please Enter a valid Password");
+
+// Quesion 03
+// Answer
+
+// ===============> alert("Welocme to Js Land... \n Happy Coding");
+
+// Quesion 04
+// Answer
+
+// ===============> alert("Welocme to Js Land...");
+
+// Question 05
+// Answer
+
+// ===============> alert("Happy Coding");
+
+// Question 06
+// Answer
+
+// ===============> alert("Hello... I can run JS through my web browser's console");
+
+// ======================================== Chapter 02 ======================================== ✅
+
+// Chapter 02
+
+// Question 01
+// Answer
+
+// ===============> var username = Jhon Deo;
+
+// Question 02
+// Answer
+
+// ===============> var myName = Muhammad Umer Farooq;
+
+// Question 03
+// Answer
+
+// (A) ===============> var titled;
+// (B) ===============> titled = "Hello World";
+// (C) ===============> alert(titled);
+
+// Question 04
+// Answer
+// ===============> var name = Jhon Deo;
+// ===============> var age = 22;
+// ===============> var course = "Certified Mobile Application Development";
+// ===============> alert(name);
+// ===============> alert(age);
+// ===============> alert(course);
+
+// Question 05
+// Answer
+// ===============> var a = "PIZZA\nPIZZ\nPIZZ\nPIZ\nPI\nP";
+// ===============> alert(a);
+
+// Question 06
+// Answer
+// ===============> var email = "umerafrooq@gmail.com";
+// ===============> alert(`My email address is ${email}.`);
+
+
+// Question 07
+// Answer
+// ===============> var book = "A Smarter Way to Learn JavaScript";
+// ===============> alert(`I am trying to learn from the book "${book}".`);
+
+// Question 08
+// Answer
+// ===============> var content = "Yah! I can write HTML Content Throgh JavaScript";
+// ===============> document.write(content);
+
+// Question 09
+// Answer
+// ===============> var content = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// ===============> alert(content);
+
+// ======================================== Chapter 03 ======================================== ✅
+
+// Chapter 03
+
+// Question 01
+// Answer
+// ===============> var age = 24;
+// ===============> alert(`I am ${age} years old.`);   
+
+// Question 02
+// Answer
+
+// ===============> var number = 10;
+// ===============> alert(`You have visited this site ${number} times`);
+
+// Question 03
+// Answer
+// ===============> var birthYear = 2002;
+// ===============> document.write(`My birth Year is ${birthYear} <br> Data type of my declared variale is ${typeof(birthYear)}`);
+
+// Quesion 04
+// Answer
+
+// (A) ===============> var visitorName = "umer Farooq";
+// (B) ===============> var producttitle = "Wrist Watch";
+// (C) ===============> var quantity = 5;
+
+// ===============> document.write(`<b> ${visitorName} </b> Orderd <b> ${quantity} ${producttitle} </b> on daraz.pk`);
+
+
+
+// ======================================== ✅✅✅ END OF Assignment01 ✅✅✅ ========================================
